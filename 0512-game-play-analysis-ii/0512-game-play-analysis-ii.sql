c9642ad85a566d4917984194f9c61fd1b2dc60cb
sahithireddysabbu <149212177+sahithireddysabbu@users.noreply.github.com>
2023-10-28 15:27:51 -0500

    Time: 1046 ms (34.30%), Space: 0B (100.00%) - LeetHub

--- 0512-game-play-analysis-ii/0512-game-play-analysis-ii.sql
+++ 0512-game-play-analysis-ii/0512-game-play-analysis-ii.sql
@@ -1,5 +1,4 @@
-# Write your MySQL query statement below
-WITH CTE AS (SELECT *,DENSE_RANK() OVER(PARTITION BY m.player_id ORDER BY m.event_date) AS rnk FROM (SELECT a.* FROM activity a)m)
-SELECT c.player_id,c.device_id
-FROM CTE c
-WHERE c.rnk<2;
+SELECT player_id,device_id FROM Activity
+WHERE (player_id,event_date) IN (SELECT player_id,MIN(event_date)
+FROM Activity
+GROUP BY player_id)
