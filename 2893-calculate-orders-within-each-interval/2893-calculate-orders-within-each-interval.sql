008672161703726b30340e6dc0a8597c71d33b5c
sahithireddysabbu <149212177+sahithireddysabbu@users.noreply.github.com>
2023-11-06 19:10:20 -0600

    Time: 807 ms (9.14%), Space: 0B (100.00%) - LeetHub

--- 2893-calculate-orders-within-each-interval/2893-calculate-orders-within-each-interval.sql
+++ 2893-calculate-orders-within-each-interval/2893-calculate-orders-within-each-interval.sql
@@ -1,4 +1,4 @@
-SELECT ceiling(minute/6.0) AS interval_no,SUM(order_count)  AS total_orders
+SELECT (minute-1)DIV 6+1 AS interval_no,SUM(order_count)  AS total_orders
 FROM Orders
 GROUP BY interval_no
 ORDER BY interval_no
