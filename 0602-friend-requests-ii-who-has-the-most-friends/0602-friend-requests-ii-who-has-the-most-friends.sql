2bb66a6953894967a31e14fffedadd8c51a42128
sahithireddysabbu <149212177+sahithireddysabbu@users.noreply.github.com>
2023-11-10 12:31:30 -0600

    Time: 525 ms (58.95%), Space: 0B (100.00%) - LeetHub

---
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,7 @@
+WITH CTE AS(SELECT requester_id id FROM RequestAccepted
+           UNION ALL 
+           SELECT accepter_id id FROM RequestAccepted)
+SELECT id,COUNT(*) AS num FROM CTE
+GROUP BY 1
+ORDER BY 2 DESC 
+LIMIT 1
