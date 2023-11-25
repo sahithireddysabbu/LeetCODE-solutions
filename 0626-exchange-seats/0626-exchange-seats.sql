b75cbd53f99383b9fc97aea254f231cf39cd2317
sahithireddysabbu <149212177+sahithireddysabbu@users.noreply.github.com>
2023-11-25 14:39:19 -0600

    Time: 704 ms (40.62%), Space: 0B (100.00%) - LeetHub

---
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -0,0 +1,9 @@
+SELECT CASE 
+            WHEN id%2<>0 and id=(SELECT COUNT(*) FROM Seat) THEN id
+            WHEN id%2=0 THEN id-1
+            ELSE id+1
+            END AS id,student
+            FROM Seat
+ORDER BY id
+
+
